d059cafeb4ecb659805bede59134a715eb390bc0
Gabriel Godoy <gabrielsantosgodoy1998@gmail.com>
2025-11-19 18:58:10 -0300

    feat: Estrutura inicial do projeto e conexão com MongoDB

---
+++ src/routes/index.js
@@ -0,0 +1,9 @@
+import express from "express"
+
+const routes = (app) => {
+    app.route("/").get((req, res) => res.status(200).send("PetShop Home"))
+
+    app.use(express.json())
+}
+
+export default routes;
