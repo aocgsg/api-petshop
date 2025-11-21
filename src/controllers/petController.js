52dfef3d3627104bcff0f16784fb7d9d3dc1fcdd
Gabriel Godoy <gabrielsantosgodoy1998@gmail.com>
2025-11-21 14:44:37 -0300

    feat: Adicionamos put e delete para o Pet

--- src/controllers/petController.js
+++ src/controllers/petController.js
@@ -11,12 +11,42 @@ class petController {
         }
     }
 
+    static async listarPetsPorEspecie(req, res) {
+        const especie = req.query.especie;
+        try {
+            const petPorEspecie = await pet.find({ "especie": especie })
+            res.status(200).json(petPorEspecie);
+        } catch (error) {
+            res.status(500).json({ message: "Erro ao listar pets por especie", error });
+        }
+    }
+
     static async cadastrarPet(req, res) {
         try {
             const novoPet = await pet.create(req.body);
             res.status(201).json({ message: "Pet cadastrado com sucesso", novoPet });
         } catch (error) {
             res.status(500).json({ message: "Erro ao cadastrar pet", error });
+        }
+    }
+
+    static async atualizarPet(req, res) {
+        try {
+            const id = req.params.id
+            await pet.findByIdAndUpdate(id, req.body)
+            res.status(200).send("Pet atualizado com sucesso!")
+        } catch (error) {
+            res.status(500).json({ message: "Erro ao atualizar pet", error });
+        }
+    }
+
+    static async removerPet(req, res) {
+        try {
+            const id = req.params.id
+            await pet.findByIdAndDelete(id)
+            res.status(200).send("Deletado com sucesso!")
+        } catch (error) {
+            res.status(500).json({ message: "Erro ao deletar pet", error });
         }
     }
 }
--- src/routes/petRoutes.js
+++ src/routes/petRoutes.js
@@ -4,6 +4,9 @@ import petController from "../controllers/petController.js";
 const routes = express.Router()
 
 routes.get("/pets", petController.listarPets);
+routes.get("/pets/busca", petController.listarPetsPorEspecie);
 routes.post("/pets", petController.cadastrarPet);
+routes.put("/pets/:id", petController.atualizarPet);
+routes.delete("/pets/:id", petController.removerPet);
 
 export default routes;
